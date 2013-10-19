88b1a72fa637f6aec58dffab87ded94552a7a6a4
patrykfischer <fischer.patryk@gmail.com>
2013-10-19 22:43:51 +0200

    Add function Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,21 @@ class Product < ActiveRecord::Base
   attr_accessible :title, :description, :image_url, :price
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :image_url, format: {with: /\A(\w)+\.(gif|png|bmp)\z/}
+
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private ##jeżeli produkt jest w koszyku to nie można go usunać z bazy danych
+	  def ensure_not_referenced_by_any_line_item
+		if line_items.empty?
+			return true
+		else
+			errors.add(:base, 'Line Items present')
+			return false
+		end
+	end
+
+
+
 end
--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,15 +1,13 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
   test "should create product with all valid data" do
-  	assert Product.new(description:"blabla", image_url:"tee", price: "0.01", title: "dupa").valid?
+  	assert Product.new(description:"blabla", image_url:"tee.png", price: "0.01", title: "dupa").valid?
   end
 
   test "should not create product without description" do
-	assert !Product.new(image_url:"tee", price: "0.01", title: "dupa").valid?
+		assert !Product.new(image_url:"tee", price: "0.01", title: "dupa").valid?
   end
 
   test "should not create product without right price" do
@@ -26,11 +24,13 @@ class ProductTest < ActiveSupport::TestCase
   	assert p.errors[:title].any?
   end
 
-  test "should create product without image_url" do
-  	p =Product.new(price: "10.00", description: " dsdsds", title: " deed")
+  test "should create product without valid image_url" do
+  	p =Product.new(price: "10.00", description: "dsdsds", title: " deed")
+  	assert ! p.valid?
+  	assert p.errors[:image_url].any?
+  	p = Product.new(price: "10.00", description: "dlsfjfd", title: "kfdsjddfsd", image_url: "kfdjsf")
   	assert ! p.valid?
   	assert p.errors[:image_url].any?
   end
-
 
 end 
